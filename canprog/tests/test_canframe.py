2b071331dd3cfffb5679f032e6c0124df791cd10
Marcin Borowicz <marcin_bor@wp.pl>
2017-10-26 23:35:12 +0200

    some tests

--- canprog/tests/test_canframe.py
+++ canprog/tests/test_canframe.py
@@ -5,10 +5,31 @@ Created on 26.10.2017
 '''
 import unittest
 
+import canbus
+
 class Test(unittest.TestCase):
 
-    def testName(self):
-        assert(False)
+    def testConstructor(self):
+        with self.assertRaises(TypeError):
+            canbus.CanFrame()
+        
+        with self.assertRaises(ValueError):
+            canbus.CanFrame(mid=-1)
+            
+        with self.assertRaises(ValueError):
+            canbus.CanFrame(mid=-1, extended=True)
+        
+        with self.assertRaises(ValueError):
+            canbus.CanFrame(mid=0x800)
+        
+        with self.assertRaises(ValueError):
+            canbus.CanFrame(mid=0x20000000, extended=True)
+    
+        f = canbus.CanFrame(mid=0, dlc=2, remote=True)
+        self.assertEqual(f.mid, 0, 'wrong message id')
+        self.assertEqual(f.dlc, 2, 'wrong dlc')
+        self.assertEqual(f.is_remote, True, 'wrong remote type')
+            
 
 
 if __name__ == "__main__":
