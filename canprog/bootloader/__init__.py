1725561d619db71b41f4a55494bbac80d17d4dab
Marcin Borowicz <marcin_bor@wp.pl>
2017-10-28 15:21:18 +0200

    add class selector

--- canprog/bootloader/__init__.py
+++ canprog/bootloader/__init__.py
@@ -1,1 +1,11 @@
-from .common import AbstractFileLoader
+from .common import AbstractFileManager
+
+from . import files
+
+def get_file_manager_class_by_name(name):
+    if name == 'hex':
+        return files.HexFileManager
+    elif name == 'bin':
+        return files.BinFileManager
+    else:
+        raise NotImplementedError('unknown file format type')
