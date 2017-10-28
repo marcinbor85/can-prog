94e3dd4bd002db50c337e36eb6390ee57719be97
Marcin Borowicz <marcin_bor@wp.pl>
2017-10-28 19:40:27 +0200

    names refactoring

--- canprog/protocols/abstract.py
+++ canprog/protocols/abstract.py
@@ -4,18 +4,49 @@ Created on 23.10.2017
 @author: mborowicz
 '''
 
-from canprog.canbus import AbstractCAN
+import can 
 
+import sys
+import time
+
+if 'win' in sys.platform.lower():
+    current_time = time.clock
+else:
+    current_time = time.time
+    
 class AbstractProtocol(object):
     '''
     classdocs
     '''
 
-
     def __init__(self, iface):
-        if not isinstance(iface, AbstractCAN):
+        if not isinstance(iface, can.BusABC):
             raise Exception('canbus interface not compatible')
         self._iface = iface
+        
+    def _send(self, msg):
+        try:
+            self._iface.send(msg)
+        except can.CanError:
+            raise IOError('Initial message sending error')
+    
+    def _recv(self, timeout=1.0, checker=None):
+        timeout = 1.0
+        time = current_time()
+        response = None
+        while current_time() - time < timeout:
+            frame = self._iface.recv(0.0)
+            if frame:
+                if checker != None:
+                    if not checker(frame):
+                        continue                
+                response = frame
+                break
+
+        if not response:
+            raise TimeoutError('Connecting timeout')
+        
+        return response
         
     def connect(self):
         raise NotImplementedError('not implemented yet')
@@ -40,6 +71,4 @@ class AbstractProtocol(object):
     
     def go(self):
         raise NotImplementedError('not implemented yet')
-
-    def info(self):
-        raise NotImplementedError('not implemented yet')    
+ 
