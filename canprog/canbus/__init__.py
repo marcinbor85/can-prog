7e3c3d8fb3d1633caeffce93f88a6b01a50d721b
Marcin Borowicz <marcin_bor@wp.pl>
2017-10-28 19:39:24 +0200

    canbus interfaces layer deleted

--- canprog/canbus/__init__.py
+++
@@ -1,9 +0,0 @@
-from .common import CanFrame, AbstractCAN
-
-from . import socketcan
-
-def get_can_interface_class_by_name(name):
-    if name == 'socketcan':
-        return socketcan.SocketCAN
-    else:
-        raise NotImplementedError('unknown can interface type')
