f5cac3bb256f35b3394023ffb78f50502bcf6f86
Marcin Borowicz <marcin_bor@wp.pl>
2017-10-28 15:22:21 +0200

    class selectors

--- canprog/protocols/__init__.py
+++ canprog/protocols/__init__.py
@@ -1,1 +1,9 @@
 from .common import AbstractProtocol
+
+from . import stm32
+
+def get_protocol_class_by_name(name):
+    if name == 'stm32':
+        return stm32.STM32Protocol
+    else:
+        raise NotImplementedError('unknown protocol type')
