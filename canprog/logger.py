0f94aa34f01497df160ead0eb67b2ba6ce216fc9
Marcin Borowicz <marcin_bor@wp.pl>
2017-10-28 19:40:36 +0200

    logger improvements

--- canprog/logger.py
+++ canprog/logger.py
@@ -1,3 +1,21 @@
 import logging
 
-log = logging.getLogger('canprog')
+from canprog import __appname__
+
+def get_log(name):
+    log = logging.getLogger(name)
+    h = logging.StreamHandler()
+    formatter = logging.Formatter('[%(module)-8s] [%(asctime)s,%(msecs)03d] [%(levelname)-8s] - %(message)s',datefmt='%H.%M.%S')
+    h.setFormatter(formatter)
+    log.addHandler(h)    
+    return log
+
+log = get_log(__appname__)
+
+def set_level(level):
+    log.setLevel(level)
+    for h in log.handlers:
+        h.setLevel(level)
+
+set_level(logging.INFO)
+
