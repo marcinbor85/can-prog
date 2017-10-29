2bccc323214dc8248f6a3c15d3c9f034952c2579
Marcin Borowicz <marcin_bor@wp.pl>
2017-10-29 13:15:46 +0100

    description and header added

--- canprog/__init__.py
+++ canprog/__init__.py
@@ -1,4 +1,11 @@
+'''
+Created on 23.10.2017
+
+@author: Marcin Borowicz <marcinbor85@gmail.com>
+
+'''
 
 __version__ = '0.1.0-alpha'
 __appname__ = 'canprog'
+__description__ = 'Command-line tool to flashing devices by CAN-BUS.'
 
